8ddab3d4a0c6be370412c4257aed798ec6226d4d
postmalloc <hi@srimukh.com>
2020-12-31 02:02:20 +0530

    buttons and demos

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # slowbug
+<img src="./images/banner.jpg" width="400px">
+
 Slowbug is a VS Code extension for debugging your code by running it slowly, at your pace.
+
+## What?
+The idea is simple. Make it possible to see how the control 
+flows through the code. I noticed that often, I end up inserting 
+too many breakpoints, and find it tedious to step through and
+understand the flow when working on a new codebase. Slowbug aims
+to solve it by executing the code slowly, at your preferred speed,
+line by line. Think of it like the "step over" button of any debugger,
+but automatic.
+
+Slowbug is agnostic to languages and debug adapters since it directly
+issues commands to the VS Code workbench.
+
+## Usage
+<img src="./images/slowbug_demo.gif" width="500px">    
+
+The functionality right now is minimal. You can pause and resume Slowbug.
+The speed can be configured in `settings > Slowbug > stepDuration`.
+
+
+## Upcoming features
+1. Range selection  
+Ability to select a code block and enable Slowbug only there.
+2. Step into functions  
+Ability to step into functions for a certain depth.
+
+## License
+MIT License
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,28 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-	let v = vscode.commands.registerCommand('extension.slowbug-start', () => {
-		vscode.commands.executeCommand("workbench.action.debug.start");
-		setInterval(() => vscode.commands.executeCommand("workbench.action.debug.stepOver"), 1000);
-	});
-	context.subscriptions.push(v);
+  let timerId: any;
+  let stepDuration = vscode.workspace.getConfiguration('slowbug').stepDuration;
+
+	const startFn = () => {
+    if(vscode.debug.activeDebugSession == undefined){
+      stepDuration = vscode.workspace.getConfiguration('slowbug').stepDuration;
+      vscode.commands.executeCommand("workbench.action.debug.start");
+    }
+    let stepFn = () => vscode.commands.executeCommand("workbench.action.debug.stepOver");
+		timerId = setInterval(stepFn, stepDuration);
+	}
+
+  const pauseFn = () => {
+		vscode.commands.executeCommand("workbench.action.debug.pause");
+    clearInterval(timerId);
+  }
+
+  let cmds: any = [];
+	const startCmd = vscode.commands.registerCommand('extension.slowbug-start', startFn);
+	const pauseCmd = vscode.commands.registerCommand('extension.slowbug-pause', pauseFn);
+  cmds = [startCmd, pauseCmd];
+  context.subscriptions.push(...cmds);
 }
 
-export function deactivate() { }
+export function deactivate() {}
