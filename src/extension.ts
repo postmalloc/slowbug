1c4a49af44f8b40e7e77ceccf1deb1e68b716f36
postmalloc <hi@srimukh.com>
2020-12-31 02:08:35 +0530

    fix indent

--- src/extension.ts
+++ src/extension.ts
@@ -4,23 +4,23 @@ export function activate(context: vscode.ExtensionContext) {
   let timerId: any;
   let stepDuration = vscode.workspace.getConfiguration('slowbug').stepDuration;
 
-	const startFn = () => {
+  const startFn = () => {
     if(vscode.debug.activeDebugSession == undefined){
       stepDuration = vscode.workspace.getConfiguration('slowbug').stepDuration;
       vscode.commands.executeCommand("workbench.action.debug.start");
     }
     let stepFn = () => vscode.commands.executeCommand("workbench.action.debug.stepOver");
-		timerId = setInterval(stepFn, stepDuration);
-	}
+    timerId = setInterval(stepFn, stepDuration);
+  }
 
   const pauseFn = () => {
-		vscode.commands.executeCommand("workbench.action.debug.pause");
+    vscode.commands.executeCommand("workbench.action.debug.pause");
     clearInterval(timerId);
   }
 
   let cmds: any = [];
-	const startCmd = vscode.commands.registerCommand('extension.slowbug-start', startFn);
-	const pauseCmd = vscode.commands.registerCommand('extension.slowbug-pause', pauseFn);
+  const startCmd = vscode.commands.registerCommand('extension.slowbug-start', startFn);
+  const pauseCmd = vscode.commands.registerCommand('extension.slowbug-pause', pauseFn);
   cmds = [startCmd, pauseCmd];
   context.subscriptions.push(...cmds);
 }
