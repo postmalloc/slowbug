fa5cc5898bb8c3ffcc554c947479768a87e485d9
postmalloc <hi@srimukh.com>
2021-01-01 16:26:59 +0530

    better handling of timers and termination conditions

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,34 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  let timerId: any;
   let stepDuration = vscode.workspace.getConfiguration('slowbug').stepDuration;
-
+  let stepFn = () => {
+    vscode.commands.executeCommand("workbench.action.debug.stepInto");
+  }
   const startFn = () => {
-    if(vscode.debug.activeDebugSession == undefined){
+    if (vscode.debug.activeDebugSession == undefined) {
       stepDuration = vscode.workspace.getConfiguration('slowbug').stepDuration;
       vscode.commands.executeCommand("workbench.action.debug.start");
+      if (context.globalState.get('timerId') == undefined) {
+        vscode.debug.onDidStartDebugSession(() => {
+          context.globalState.update('timerId', setInterval(stepFn, stepDuration));
+        });
+      }
+    } else {
+      setImmediate(() => context.globalState.update('timerId', setInterval(stepFn, stepDuration)));
     }
-    let stepFn = () => vscode.commands.executeCommand("workbench.action.debug.stepOver");
-    timerId = setInterval(stepFn, stepDuration);
   }
+
+  vscode.debug.onDidTerminateDebugSession(() => {
+    let timerId: any = context.globalState.get('timerId');
+    clearInterval(timerId);
+  });
 
   const pauseFn = () => {
     vscode.commands.executeCommand("workbench.action.debug.pause");
+    let timerId: any = context.globalState.get('timerId');
     clearInterval(timerId);
   }
-
   let cmds: any = [];
   const startCmd = vscode.commands.registerCommand('extension.slowbug-start', startFn);
   const pauseCmd = vscode.commands.registerCommand('extension.slowbug-pause', pauseFn);
@@ -25,4 +36,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(...cmds);
 }
 
-export function deactivate() {}
+export function deactivate() { }
