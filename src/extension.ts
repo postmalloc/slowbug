7751f09a89629be48c8eaecdf6cc515d4e42a290
postmalloc <hi@srimukh.com>
2021-01-01 16:45:19 +0530

    refactor and comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,54 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  // Read the stepDuration from the settings
   let stepDuration = vscode.workspace.getConfiguration('slowbug').stepDuration;
-  let stepFn = () => {
+
+  function stepFn() {
     vscode.commands.executeCommand("workbench.action.debug.stepInto");
   }
-  const startFn = () => {
+
+  function startFn() {
+    // Check if there is an active debug session. If not, start debugging
+    // and start the timers
     if (vscode.debug.activeDebugSession == undefined) {
       stepDuration = vscode.workspace.getConfiguration('slowbug').stepDuration;
       vscode.commands.executeCommand("workbench.action.debug.start");
       if (context.globalState.get('timerId') == undefined) {
+        // Wait until the session is ready before you start the timer
         vscode.debug.onDidStartDebugSession(() => {
-          context.globalState.update('timerId', setInterval(stepFn, stepDuration));
+          let timerId = setInterval(stepFn, stepDuration);
+          context.globalState.update('timerId', timerId);
         });
       }
     } else {
-      setImmediate(() => context.globalState.update('timerId', setInterval(stepFn, stepDuration)));
+      /* If already debugging, just create a new one
+        NOTE: The `timerId` is actually a cyclic object that 
+        cannot be JSON-stringified. Setting properties on the
+        globalState requires JSON-stringified values. But this
+        works for some reason. Probably not safe. */ 
+      let timerId = setInterval(stepFn, stepDuration);
+      setImmediate(() => context.globalState.update('timerId', timerId));
     }
   }
 
+  // Clear the timers on debug end
   vscode.debug.onDidTerminateDebugSession(() => {
     let timerId: any = context.globalState.get('timerId');
     clearInterval(timerId);
   });
 
-  const pauseFn = () => {
+  // Fn to pause the debugger and clear the current timer
+  function pauseFn() {
     vscode.commands.executeCommand("workbench.action.debug.pause");
     let timerId: any = context.globalState.get('timerId');
     clearInterval(timerId);
   }
-  let cmds: any = [];
+
   const startCmd = vscode.commands.registerCommand('extension.slowbug-start', startFn);
   const pauseCmd = vscode.commands.registerCommand('extension.slowbug-pause', pauseFn);
-  cmds = [startCmd, pauseCmd];
-  context.subscriptions.push(...cmds);
+
+  context.subscriptions.push(startCmd, pauseCmd);
 }
 
 export function deactivate() { }
